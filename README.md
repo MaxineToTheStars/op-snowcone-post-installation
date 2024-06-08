61e4f23a8700daf844be5f279932fdb350391739
Maxeeme <146043018+MaxineToTheStars@users.noreply.github.com>
2024-06-08 15:26:57 -0400

    Major changes
    + Updated file header
    + Added CONFIG_APPS_EXTERNAL_INSTALL for installing APKs not found in F-Droid
    + Added Magisk
    + Added QUIK SMS
    + Added Fossify Voice Recorder
    + Revamped the whole installing process. Should be a lot cleaner+smoother
    ~ Modified the README to have more GIFs!!!111!!
    ~ Modified default wallpaper
    - Removed termux.widget, typeblog.shelter, and fossify.messages
    - Removed SYSTEM_SETTINGS
    
    A lot of code cleanup + other stuff

--- README.md
+++ README.md
@@ -5,9 +5,14 @@
 <h1 align="center">OP Snow-cone Post-Install</h1>
 
 <!-- Subheading -->
-<h3 align="center">Post-installation script</h3>
+<h3 align="center">A simple post-installation script</h3>
+
+<!-- Move text down -->
+<br>
 
 <!-- GIF -->
 <p align="center">
-  <img src="https://c.tenor.com/JFVk98vql5gAAAAd/tenor.gif"/>
+  <img src="https://media1.tenor.com/m/BnjcVvUXASYAAAAC/lineageos-custom-rom.gif"/>
+  <img src="https://media1.tenor.com/m/lTZ3JUXbuJIAAAAC/oneplus-oppo.gif"/>
+  <img src="https://media1.tenor.com/m/r2JQ7OID2FIAAAAd/linux.gif">
 </p>
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-# op-snowcone-post-install || run.sh
+# op-snowcone-post-install || install.sh
 # --------------------------------
 # Post installation script.
 #
@@ -19,6 +19,10 @@ declare -r -a CONFIG_APPS_DISABLE=(
     "com.android.gallery3d"
     "com.android.messaging"
 )
+declare -r -a CONFIG_APPS_EXTERNAL_INSTALL=(
+    "https://github.com/ReVanced/revanced-manager/releases/download/v1.20.1/revanced-manager-v1.20.1.apk"
+    "https://github.com/xManager-App/xManager/releases/latest/download/xManager.apk"
+)
 declare -r -a CONFIG_APPS_INSTALL=(
     "app.lawnchair"
     "app.organicmaps"
@@ -27,31 +31,31 @@ declare -r -a CONFIG_APPS_INSTALL=(
     "com.bnyro.clock"
     "com.darkempire78.opencalculator"
     "com.fsck.k9"
+    "com.gitlab.mudlej.MjPdfReader"
     "com.hjiangsu.thunder"
     "com.looker.droidify"
     "com.nononsenseapps.feeder"
     "com.termux"
-    "com.termux.widget"
+    "com.topjohnwu.magisk"
     "com.x8bit.bitwarden"
+    "dev.octoshrimpy.quik"
     "helium314.keyboard"
     "io.anuke.mindustry"
     "me.jmh.authenticatorpro"
     "me.zhanghai.android.files"
     "net.minetest.minetest"
     "net.sourceforge.opencamera"
-    "net.typeblog.shelter"
     "org.cromite.cromite"
     "org.fossify.calendar"
     "org.fossify.contacts"
     "org.fossify.gallery"
-    "org.fossify.messages"
     "org.fossify.phone"
+    "org.fossify.voicerecorder"
     "org.jellyfin.mobile"
     "org.joinmastodon.android.moshinda"
     "org.mozilla.fennec_fdroid"
     "org.videolan.vlc"
     "org.wikipedia"
-    "website.leifs.delta.foss"
 )
 declare -r -a CONFIG_APPS_REMOVE=(
     "org.lineageos.audiofx"
@@ -63,31 +67,88 @@ declare -r -a CONFIG_APPS_REMOVE=(
 
 # Constants
 declare -r CONST_ROOT_DIRECTORY=$PWD
-declare -r -a CONST_SYSTEM_SETTINGS=( "ACCELEROMETER_ROTATION 0" )
 
 # Main
 function main() {
+    # Start the ADBd Server
     _internal_enable_adb
+
+    # Install third-party applications
     _internal_install_third_party_apps
+
+    # Modify Android settings
     _internal_modify_android_settings
-    echo "Done!"
 }
 
-# Enabled ADB and launches as root
+# Enables ADB and launches as root
 function _internal_enable_adb() {
     # Validate we are in the root project directory
     cd $CONST_ROOT_DIRECTORY
 
+    # Log
+    echo "[LOG] Enabling ADB..."
+
     # Start the ADBd Server
-    adb start-server
-    adb kill-server
-    adb start-server
+    adb start-server > /dev/null 2>&1
+    adb kill-server  > /dev/null 2>&1
+    adb start-server > /dev/null 2>&1
 
     # Wait for authorization
-    read -n 1 -p "Press Enter Once Authorized: "
+    read -n 1 -p "[ACTION] Authorize USB Debugging then press Enter: "
 
     # Connect as root
-    adb root
+    adb root > /dev/null 2>&1
+
+    # Log
+    echo "[1/3] ADB enabled and started as root!"
+}
+
+# Installs all third party apps
+function _internal_install_third_party_apps() {
+    # Validate we are in the root project directory
+    cd $CONST_ROOT_DIRECTORY
+
+    # Log
+    echo "[LOG] Adding external F-Droid repositories..."
+
+    # Add repositories
+    ./ctl repo add bitwarden https://mobileapp.bitwarden.com/fdroid/repo > /dev/null 2>&1
+    ./ctl repo add cromite https://www.cromite.org/fdroid/repo           > /dev/null 2>&1
+    ./ctl repo add izzyondroid https://apt.izzysoft.de/fdroid/repo       > /dev/null 2>&1
+
+    # Update
+    ./ctl update > /dev/null 2>&1
+
+    # Log
+    echo "[LOG] Installing applications..."
+
+    # Iterate through list
+    for appID in "${CONFIG_APPS_INSTALL[@]}"; do
+        # Install via app ID
+        ./ctl install $appID
+
+        # Log
+        echo "[LOG] Installed: $appID"
+    done
+
+    # Log
+    echo "[LOG] Installing external applications..."
+
+    # Iterate through the list
+    for appURL in "${CONFIG_APPS_EXTERNAL_INSTALL[@]}"; do
+        # Download
+        wget --output-file=./resources/out.apk --verbose --relative $appURL
+
+        # Install
+        adb install ./resources/out.apk > /dev/null 2>&1
+
+        # Log
+        echo "[LOG] Installed: $appURL"
+    done
+
+
+    # Log
+    echo "[2/3] Done installing applications!"
 }
 
 # Modifies internal Android settings
@@ -95,64 +156,58 @@ function _internal_modify_android_settings() {
     # Validate we are in the root project directory
     cd $CONST_ROOT_DIRECTORY
 
-    # Start the new gallery app first so the user provides the needed permissions
-    adb shell am start org.fossify.gallery/.activities.MainActivity
+    # Log
+    echo "[LOG] Opening gallery application..."
 
-    # Wait for authorization
-    read -n 1 -p "Press Enter Once Granted: "
+    # Start the new gallery app first so the user grants the needed media permissions
+    adb -d shell monkey -p org.fossify.gallery 1 > /dev/null 2>&1
 
-    # Change wallpaper
-    adb push "./resources/studio_ghibli_cat.png" "/storage/emulated/0/Download/"
+    # Wait for user confirmation
+    read -n 1 -p "[ACTION] Grant ALL file access then press Enter: "
+
+    # Kill the application
+    adb shell am force-stop org.fossify.gallery > /dev/null 2>&1
+
+    # Log
+    echo "[LOG] Uploading wallpaper..."
+
+    # Upload wallpaper to phone
+    adb push "./resources/wallpaper.png" "/storage/emulated/0/Download/wallpaper.png" > /dev/null 2>&1
+
+    # Change the wallpaper
     adb shell am start \
     -a android.intent.action.ATTACH_DATA \
     -c android.intent.category.DEFAULT \
-    -d file:///storage/emulated/0/Download/studio_ghibli_cat.png \
-    -t 'image/*' \
-    -e mimeType 'image/*'
+    -d file:///storage/emulated/0/Download/wallpaper.png \
+    -t "image/*" \
+    -e mimeType "image/*" > /dev/null 2>&1
 
-    # Wait for authorization
-    read -n 1 -p "Press Enter Once Applied: "
+    # Wait for user confirmation
+    read -n 1 -p "[ACTION] Apply the wallpaper then press Enter: "
 
-    # Iterate through system settings
-    for setting in "${CONST_SYSTEM_SETTINGS[@]}"; do
-        # Modify setting
-        adb shell settings put system ${setting,,}
-    done
+    # Log
+    echo "[LOG] Removing non-critical apps..."
 
     # Remove unended apps
     for app in "${CONFIG_APPS_REMOVE[@]}"; do
         # Remove
-        adb uninstall --user 0 $app
+        adb uninstall --user 0 $app > /dev/null 2>&1
     done
+
+    # Log
+    echo "[LOG] Disabling replaced system apps..."
 
     # Disable some system apps
     for app in "${CONFIG_APPS_DISABLE[@]}"; do
         # Clear data
-        adb shell pm clear $app
+        adb shell pm clear $app > /dev/null 2>&1
 
         # Disable
-        adb shell pm disable $app
+        adb shell pm disable $app > /dev/null 2>&1
     done
-}
 
-# Installs all third party apps located in /resources
-function _internal_install_third_party_apps() {
-    # Validate we are in the root project directory
-    cd $CONST_ROOT_DIRECTORY
-
-    # Add repositories
-    ./ctl repo add bitwarden https://mobileapp.bitwarden.com/fdroid/repo
-    ./ctl repo add cromite https://www.cromite.org/fdroid/repo
-    ./ctl repo add izzyondroid https://apt.izzysoft.de/fdroid/repo
-
-    # Update
-    ./ctl update
-
-    # Iterate through list
-    for appID in "${CONFIG_APPS_INSTALL[@]}"; do
-        # Install via app ID
-        ./ctl install $appID
-    done
+    # Log
+    echo "[3/3] Done with system modifications!"
 }
 
 # Execute
